7490088209d2328b26ee685202b8d5505a7ff3d1
Kiran_Kutte <kirankutte28@gmail.com>
2024-02-14 16:17:39 +0530

    Spring demo

---
+++ src/main/java/com/example/demo/scalerClass.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class scalerClass {
+
+    @GetMapping("/hi")
+    public String sayHello(){
+        return "Hi ! How are you?, I am Kiran" ;
+    }
+}
